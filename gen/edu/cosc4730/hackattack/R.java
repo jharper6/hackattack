bdeb5ad541103749b6fa923bc748dc6081f8a4af
storm128 <cholton2@uwyo.edu>
2014-11-30 16:20:44 -0700

    Edited layouts for results and pass screen

--- gen/edu/cosc4730/hackattack/R.java
+++ gen/edu/cosc4730/hackattack/R.java
@@ -25,8 +25,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080010;
-        public static final int btn_continue=0x7f08000f;
+        public static final int action_settings=0x7f080012;
+        public static final int btn_continue=0x7f080010;
         public static final int btn_lnch2pwr=0x7f080006;
         public static final int btn_lnch4pwr=0x7f080004;
         public static final int btn_prob1=0x7f080002;
@@ -34,11 +34,13 @@ public final class R {
         public static final int btn_steal=0x7f080008;
         public static final int btn_test=0x7f080001;
         public static final int fragment_container=0x7f080000;
+        public static final int output=0x7f080011;
+        public static final int textViewP2=0x7f08000e;
+        public static final int textViewPassP2=0x7f08000f;
         public static final int tv_cost2pwr=0x7f080007;
         public static final int tv_cost4pwr=0x7f080005;
         public static final int tv_coststeal=0x7f080009;
         public static final int tv_movesleft=0x7f08000b;
-        public static final int tv_passText=0x7f08000e;
         public static final int tv_player1score=0x7f08000c;
         public static final int tv_player2score=0x7f08000d;
         public static final int tv_playerturn=0x7f08000a;
