291a14cde030d13ffd7a95473f4ff395d499f1dc
jharper6 <jharper6@uwyo.edu>
2014-11-30 14:08:08 -0700

    Fixed Swap.

--- gen/edu/cosc4730/hackattack/R.java
+++ gen/edu/cosc4730/hackattack/R.java
@@ -25,10 +25,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
+        public static final int action_settings=0x7f080005;
+        public static final int btn_continue=0x7f080003;
         public static final int btn_test=0x7f080001;
         public static final int fragment_container=0x7f080000;
-        public static final int resultView1=0x7f080002;
+        public static final int resultView1=0x7f080004;
+        public static final int tv_passText=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/edu/cosc4730/hackattack/PassFragment.java
+++ src/edu/cosc4730/hackattack/PassFragment.java
@@ -1,11 +1,13 @@
 package edu.cosc4730.hackattack;
 
 import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class PassFragment extends Fragment {
 	
@@ -19,11 +21,31 @@ public class PassFragment extends Fragment {
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
         Bundle savedInstanceState) {
-        View myView = inflater.inflate(R.layout.fragment_board, container, false);
+        View myView = inflater.inflate(R.layout.fragment_pass, container, false);
         // TODO create buttons
         // TODO add listeners
         myView.setBackgroundColor(Color.RED);
+        
+        Button btnPass = (Button) myView.findViewById(R.id.btn_continue);
+        btnPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              swapFrag();
+            }
+          });
+        
         return myView;
+	}
+	
+	public void swapFrag() {
+		
+		BoardFragment boardFragment = new BoardFragment();
+		FragmentTransaction transaction = getFragmentManager().beginTransaction();
+		
+		transaction.replace(R.id.fragment_container, boardFragment);
+		transaction.addToBackStack(null);
+		
+		transaction.commit();
 	}
 
 }
